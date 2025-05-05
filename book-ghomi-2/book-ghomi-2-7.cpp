6fa241d4b34fec77a52b377595c15ce21c80846c
mshajkarami <mshajkarami@gmail.com>
2025-05-05 18:03:24 +0330

    book-ghomi-2-7

---
+++ book-ghomi-2/book-ghomi-2-7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath> // for pow(), though it's not used here
+
+using namespace std;
+
+int main() {
+    int w;
+    printf("Please enter a number of weight:\n");
+    scanf("%d", &w);
+    printf("Weight = %d\n", w * 10);
+    return 0;
+}
